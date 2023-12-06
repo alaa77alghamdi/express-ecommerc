08615a3af35c4fd9c2c1d17dd10e517cc8cb1210
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-12-06 12:57:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3005;
+
+app.get("/products", (req, res) => {
+  res.send({
+    products: [
+      { id: 1, title: iphone12, price: 5000 },
+      { id: 2, title: iphone13, price: 5000 },
+      { id: 3, title: iphone14, price: 5000 },
+    ],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`run at server${port}`);
+});
